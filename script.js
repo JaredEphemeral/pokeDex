52cf3822a6319f8b68c8d605d26c9ad9d0c5b6e1
Jared Ephemeral <jared.menrod@gmail.com>
2022-08-31 20:24:47 -0600

    actualizacion

--- script.js
+++ script.js
@@ -1,58 +1,122 @@
 const pokeCard = document.querySelector('[pokeCard]');
-const pokeName = document.querySelector('[nombrePokemon]');
+const pokeNombre = document.querySelector('[nombrePokemon]');
 const pokeImg = document.querySelector('[imgPokemon]');
 const pokeId = document.querySelector('[idPokemon]');
-const pokeTypes = document.querySelector('[tiposPokemon]');
+const pokeTipos = document.querySelector('[tiposPokemon]');
 const pokeHp = document.querySelector('[pokemonHP]');
 const PokeAtk = document.querySelector('[pokemonAtk]');
 const pokeDef = document.querySelector('[pokemonDef]');
 const PokeSp = document.querySelector('[pokemonSP]');
 const PokeSd = document.querySelector('[pokemonSD]');
 const PokeSpeed = document.querySelector('[pokemonSpeed]');
-
 const defaultValue = 1;
+
+
+const catalogoColores = {
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    grass: '#4A9681',
+    electric: '#FFEA70',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F'
+}
+
+const catalogoTipos ={
+    normal: 'Normal',
+    fire: 'Fuego',
+    water: 'Agua',
+    grass: 'Hierba',
+    electric: 'Eléctrico',
+    ice: 'Hielo',
+    rock: 'Roca',
+    flying: 'Volador',
+    psychic: 'Psíquico',
+    ghost: 'Fantasma',
+    bug: 'Bicho',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    dragon: 'Dragón',
+    steel: 'Metal',
+    fighting: 'Luchador'
+}
+
+function valoresDefault(){
+    pokeImg.setAttribute('src', "./pika.png")
+    pokeId.textContent = `#000`;
+    pokeNombre.textContent = `Nombre: No Encontrado`;
+    pokeTipos.textContent = "NA/NA";
+    pokeHp.textContent = `Vida: 00`;
+    PokeAtk.textContent = `Atq: 00`;
+    pokeDef.textContent = `Def: 00`;
+    PokeSp.textContent = `Atq-Esp: 00`;
+    PokeSd.textContent = `Def-Esp: 00`;
+    PokeSpeed.textContent = `Velocidad: 00`;
+    pokeImg.style.background = "";
+}
 
 const buscarPokemon = event =>{
     event.preventDefault();
     const { value } = event.target.pokeBusqueda;
     if(value == ""){
-        pokeImg.setAttribute('src', "./pika.png")
-        pokeId.textContent = `#000`;
-        pokeName.textContent = `Nombre: N/A`;
-        pokeTypes.textContent = "NA/NA";
-        pokeHp.textContent = `HP: 00`;
-        PokeAtk.textContent = `Atk: 00`;
-        pokeDef.textContent = `Def: 00`;
-        PokeSp.textContent = `SP: 00`;
-        PokeSd.textContent = `SD: 00`;
-        PokeSpeed.textContent = `Speed: 00`;
+        valoresDefault();
     }
     else{
         fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
             .then(data => data.json())
-            .then(response => pokemonData(response))
+            .then(response => informacionPokemon(response))
+            .catch( function(err){
+                valoresDefault()
+            });
     }
 }
 
-const pokemonData = data => {
-    const {stats, types, sprites} = data;
-    
-    pokeImg.setAttribute('src', data.sprites.front_default)
-    pokeId.textContent = `# ${data.id}`;
-    pokeName.textContent = `Nombre: ${mayusInicial(data.name)} `;
+function informacionPokemon (data){
+        const {stats, types, sprites} = data;
+        pokeImg.setAttribute('src', sprites.front_default)
 
-    pokeHp.textContent = `HP: ${ data.stats[0].base_stat} `;
-    PokeAtk.textContent = `ATK: ${ data.stats[1].base_stat} `;
-    pokeDef.textContent = `DEF: ${ data.stats[2].base_stat} `;
-    PokeSp.textContent = `SP: ${ data.stats[3].base_stat} `;
-    PokeSd.textContent = `SD: ${ data.stats[4].base_stat} `;
-    PokeSpeed.textContent = `Speed: ${ data.stats[5].base_stat} `;
-    pokeTypes.textContent = "";
-    for(var i = 0; i<= data.types.length -1; i++){
-        pokeTypes.textContent += mayusInicial(data.types[i].type.name) + " ";
-    }
+        pokeId.textContent = `# ${data.id}`;
+        pokeNombre.textContent = `Nombre: ${mayusInicial(data.name)} `;
 
-    console.log(data);
+        pokeHp.textContent = `Vida: ${ stats[0].base_stat} `;
+        PokeAtk.textContent = `Atq: ${ stats[1].base_stat} `;
+        pokeDef.textContent = `Def: ${ stats[2].base_stat} `;
+        PokeSp.textContent = `Atq-Esp: ${ stats[3].base_stat} `;
+        PokeSd.textContent = `Def-Esp: ${ stats[4].base_stat} `;
+        PokeSpeed.textContent = `Velocidad: ${ stats[5].base_stat} `;
+
+        pokeTipos.innerHTML = "";
+
+        colorTipos(types);
+        colorFondo(types);
+        console.log(data);
+}
+
+function colorTipos(types){
+    types.forEach(type => {
+        var texto = document.createElement("div");
+        texto.style.color = catalogoColores[type.type.name];
+        texto.textContent = catalogoTipos[type.type.name];
+        pokeTipos.appendChild(texto);
+    });
+}
+
+function colorFondo(types){
+    const colorPrincipal = catalogoColores[types[0].type.name];
+    const colorSecundario = types[1] ? catalogoColores[types[1].type.name] : catalogoColores.default;
+
+    pokeImg.style.background = `radial-gradient(${colorSecundario} 33%, ${colorPrincipal} 33%)`
+    pokeImg.style.backgroundSize = ' 5px 5px ';
 }
 
 function mayusInicial(string) {
